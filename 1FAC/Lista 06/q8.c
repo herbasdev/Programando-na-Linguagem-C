b87c6377cf2e67a786a1338b02fe4c97cb0f0142
Leandro Herbas <herbasdev@gmail.com>
2022-06-05 21:46:07 -0300

    Add 'q8'

---
+++ 1FAC/Lista 06/q8.c
@@ -0,0 +1,30 @@
+/* QUESTÃO 08: Faça um programa que leia
+um número real x e um número inteiro y.
+Em seguida, o programa deve ler 100
+números reais e calcular quantos destes
+estão no intervalo definido por [x-y,x+y]. */
+#include <stdio.h>
+void main()
+{
+  int x, y, i, n, inicio, final, quant = 0;
+
+  printf("Insira o valor x: ");
+  scanf("%d", &x);
+  printf("Insira o valor y: ");
+  scanf("%d", &y);
+
+  inicio = (x - y);
+  final = (x + y);
+
+  for(i = 1; i <= 100; i++)
+  {
+    printf("Insira um numero real: ");
+    scanf("%d", &n);
+
+    if ((n >= inicio) && (n <= final))
+    {
+    quant++;
+    }
+  }
+   printf("Quantidade de valores inseridos que estao no intervalo definido por [%d,%d]: %d", inicio, final, quant);
+}
